f21c976558d03851937f83e0a1861ef6a6e82c41
kauanne <kauanne.l@escolar.ifrn.edu.br>
2026-06-01 11:57:43 -0300

    forms

--- src/app/calcula-media/calcula-media.ts
+++ src/app/calcula-media/calcula-media.ts
@@ -1,50 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calcula-media',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './calcula-media.html',
   styleUrl: './calcula-media.scss',
 })
-
 export class CalculaMedia {
-
   protected mediaParcial: number | undefined
   protected mediaFinal: number | undefined
   protected situacao: string
+  //FormBuilder facilita a configuração do form Group
+  //# - atributo privado em tempo de compilação e execução 
+  #formBuilder = inject(FormBuilder);
+  protected formMedia: FormGroup; 
 
   constructor() {
+    this.formMedia = this.#formBuilder.group({
+      bim1: [undefined, [Validators.required, Validators.min(0), Validators.max(100)]],
+      bim2: [undefined, [Validators.required, Validators.min(0), Validators.max(100)]],
+      bim3: [undefined, [Validators.required, Validators.min(0), Validators.max(100)]],
+      bim4: [undefined, [Validators.required, Validators.min(0), Validators.max(100)]],
+    })
     this.mediaParcial = undefined
     this.mediaFinal = undefined
-    this.situacao = ''
+    this.situacao = ""
   }
 
-  calcularMediaParcial( b1: number, b2: number, b3: number, b4: number) 
-  {
+
+  calcularMediaParcial() {
+    const notas = this.formMedia.value
+
     this.mediaParcial =
-      (b1 * 2 + b2 * 2 + b3 * 3 + b4 * 3) / 10
+      (notas.bim1 * 2 + notas.bim2 * 2 + notas.bim3 * 3 + notas.bim4 * 3) / 10
 
-    this.mediaFinal = undefined
+      //if (this.mediaParcial >= 60) {
+      //  this.situacao = 'Aprovado'
+      //  this.mediaFinal = undefined
+     // }
+      //else {
+      //this.situacao = 'Reprovado'
+    //}
+  }
 
-    if (this.mediaParcial >= 60) {
+  calcularMediaFinal(Fn: number) {
+    const mp = this.mediaParcial
+    let mf: number | undefined = undefined
+
+    if (mp !== undefined) {
+      mf =  (mp + Fn) / 2
+    }
+    this.mediaFinal = mf 
+
+    if (mf !== undefined) {
+      if (mf >= 60) {  
       this.situacao = 'Aprovado'
-    } else {
-      this.situacao = 'Avaliação Final'
+    }
+      else {
+      this.situacao = 'Reprovado'
     }
   }
-
-  calcularMediaFinal(nf: number) {
-
-    if (this.mediaParcial !== undefined) {
-
-      this.mediaFinal =
-        (this.mediaParcial + nf) / 2
-
-      if (this.mediaFinal >= 60) {
-        this.situacao = 'Aprovado'
-      } else {
-        this.situacao = 'Reprovado'
-      }
-    }
-  }
+  } 
 }
